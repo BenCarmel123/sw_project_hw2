2f12d58b950ca7dfa13c24705e8c4416ca005bab
Ben Carmel <bencarmel1@mail.tau.ac.il>
2025-06-19 14:22:11 +0000

    almost finished graph

--- bonus.py
+++ bonus.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
+import matplotlib.patches as patch
 from sklearn.datasets import load_iris
 from sklearn.cluster import KMeans
 
@@ -13,15 +14,19 @@ def main():
     x_coordinates, y_coordinates = zip(*results)
     max_inertia = max(y_coordinates)
     fig, ax = plt.subplots()
-    ax.set_xlim(0, 9)
-    ax.set_xticks(np.arange(0, 11, 1))
-    ax.set_ylim(0, 3)
+    ax.set_xlim(0, None)
+    ax.set_xticks(np.arange(0, 11.5, 1))
+    ax.set_ylim(0, None)
     ax.set_yticks(np.arange(0, max_inertia * 1.15, 100))
-    ax.set_title('Elbow Method for selection of optimal "K" clusters')
-    ax.set_xlabel("K")
-    ax.set_ylabel("Average Dispersion")
-    ax.plot(x_coordinates, y_coordinates, marker='o', linestyle='-', color='blue')
-
+    font1 = {'family':'serif','color':'red','size': 14, 'style': 'italic'}
+    font2 = {'family':'serif','color':'red','size': 10, 'style': 'italic'}
+    ax.set_title('Elbow Method for selection of optimal "K" clusters', fontdict = font1)
+    ax.set_xlabel("K", fontdict=font2)
+    ax.set_ylabel("Average Dispersion", fontdict=font2)
+    ax.plot(x_coordinates, y_coordinates, linestyle='-', color='blue', label='Inertia Plot')
+    elbow = (3,results[2][1])
+    ax.grid()
+    ax.legend(['Inertia Plot', 'Elbow'])
 if __name__ == '__main__':
     main()
     plt.savefig('elbow.png')
